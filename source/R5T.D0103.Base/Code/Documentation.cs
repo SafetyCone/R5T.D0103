e94445e6db5ff7de87674308cbf3ba209ccff46f
davidcoats <david@getsafetycone.com>
2021-11-30 18:09:24 -0800

    Initial commit of source-stub.

---
+++ source/R5T.D0103.Base/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0103
+{
+    /// <summary>
+    /// ICurrentProcessStartTimeProvider and related services.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.D0103.I001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0103.I001
+{
+    /// <summary>
+    /// Default implementation for ICurrentProcessStartTimeProvider and related services..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
